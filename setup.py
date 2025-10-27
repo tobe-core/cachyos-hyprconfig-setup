6b2b287267775570028be93660c641b50b8787b7
tobe <117567068+tobe-core@users.noreply.github.com>
2025-10-27 15:24:22 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,20 @@
+import os
+
+def setup(backup):
+    if backup:
+        os.system("cp -r ~/.config ~/.config.bak")
+    
+    os.system("git clone https://github.com/tobe-core/cachyos-hyprconfig.git ~/cachyos-hyprconfig && cp -r ~/cachyos-hyprconfig/* ~/.config/ && rm ~/.config/README.md")
+    os.system("git clone https://github.com/tobe-core/PyWall.git ~/Documentos/code/PyWall && rm ~/Documentos/code/PyWall/README.md")
+    
+    os.system("rm -rf ~/cachyos-hyprconfig")
+
+    exit()
+
+print("Want to do a backup?")
+user = input("(y, n) > ")
+
+if user == "y":
+    setup(True)
+else:
+    exit()
